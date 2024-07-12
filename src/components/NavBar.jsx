838ecc5a1e92567617757096f2ee47baf9b882dc
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-07-12 00:29:24 -0700

    redid project completely

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,137 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/logo.png";
+import { Link } from "react-scroll";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => setNav(!nav);
+
+  const hamburgerStyling =
+    "absolute top-0 left-0 w-full h-screen bg-primaryBlue flex flex-col justify-center items-center";
+
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-primaryBlue text-gray-300">
+      <div>
+        <img src={Logo} alt="Name logo" className="w-[50px]" />
+      </div>
+
+      {/* Menu */}
+      <div className="hidden md:flex">
+        <ul className="hidden md:flex">
+          <li>
+            <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={handleClick}
+              to="skills"
+              smooth={true}
+              duration={500}
+            >
+              Skills
+            </Link>
+          </li>
+          <li>
+            <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={handleClick}
+              to="contact"
+              smooth={true}
+              duration={500}
+            >
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      {/* Hamburger */}
+      <div className="md:hidden z-10" onClick={handleClick}>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul className={!nav ? "hidden" : hamburgerStyling}>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="work" smooth={true} duration={500}>
+            Portfolio
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/*Social Icons*/}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#33333]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              GitHub <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6FC2B0]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565F69]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        'neutralWhite': '#f8f9fa',
-        'neutralBlack': '#000814',
-        'secondaryBlue': '#001D3D',
-        'primaryBlue': '#003566',
-        'secondaryYellow': '#FFC300',
-        'primaryYellow': '#FFD60A',
+        'primaryBlue': '#0A192F',
       },
       screens: {
         'xs': '310px',
