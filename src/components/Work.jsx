98e693219ce160aa9c92c48fee7e8f1e8c24515e
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-09-11 12:27:19 -0700

    fix broken links

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -29,7 +29,7 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://testing-results-component.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
@@ -53,7 +53,7 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a href="https://tasty-omelette-recipe-page.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
