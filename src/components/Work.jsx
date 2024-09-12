9b8f71b5ccf8cec22537508ad4af24fa008947c4
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2024-09-11 21:04:33 -0700

    final commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -83,7 +83,10 @@ const Work = () => {
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl font-bold text-white tracking-wider"></span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://tasty-desserts-store.netlify.app/"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
