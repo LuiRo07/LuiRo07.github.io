60f663776677a8ad5b1d5d1c29df237a25b10aee
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-03-30 21:50:21 -0700

    padding added to input fields

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -20,19 +20,19 @@ const Contact = () => {
           </p>
         </div>
         <input
-          className="bg-[#ccd6f6]"
+          className="bg-[#ccd6f6] p-4"
           type="text"
           placeholder="Name"
           name="name"
         />
         <input
-          className="my-4 p-2 bg-[#ccd6f6]"
+          className="my-4 p-2 bg-[#ccd6f6] p-4"
           type="text"
           placeholder="Email"
           name="email"
         />
         <textarea
-          className="bg-[#ccd6f6] p-2"
+          className="bg-[#ccd6f6] p-4 p-2"
           name="message"
           rows="10"
           placeholder="Message"
