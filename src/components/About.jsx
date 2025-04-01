b971d6870c6de266a7ac9dc18b9eb305537886d8
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-03-31 17:43:18 -0700

    deleted unused assets/ edited about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>I'm Luis, feel free to take a look around.</p>
+            <p>I'm Luis, feel free to look around.</p>
           </div>
           <div>
             <p className="text-lg ">
