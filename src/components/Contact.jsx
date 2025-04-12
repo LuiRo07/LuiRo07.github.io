e746ea503a0affea4e2ff49a974f8c0ca087b92f
LRosas93 <71421190+LRosas93@users.noreply.github.com>
2025-04-11 22:16:16 -0700

    edited Contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,8 +15,8 @@ const Contact = () => {
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
           </p>
-          <p className="text-gray-300 py-4">
-            // Submit the form below or shoot me an email - luis_rm142@yahoo.com
+          <p className="text-gray-300 py-4 mt-2 text-xl">
+            // Let's get in touch1
           </p>
         </div>
         <input
